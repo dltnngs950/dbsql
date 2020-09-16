65d35ee488e923696d373656c14826979b1f46e9
soohun <dltnngs950@gmail.com>
2020-09-16 17:25:15 +0900

    "실습  . sub4~6" "실습  . sub6~10" "집합연산자  . UNION, UNION ALL, INTERSECT, MINUS"

--- 20200915.sql
+++ 20200915.sql
@@ -25,6 +25,15 @@ SELECT p.pid, p.pnm, :cid cid, nvl(day, 0) day, nvl(cnt, 0) cnt
 FROM cycle c, product p
 WHERE c.pid(+) = p.pid
   AND c.cid(+) = '1';
+  
+outer join 실습 5]
+
+SELECT p.pid, p.pnm, NVL(c.cid, 1) cid, NVL(m.cnm, 'brown') cnm, NVL(c.day, 0) day, NVL(c.cnt, 0) cnt
+FROM cycle c, customer m, product p
+WHERE c.pid(+) = p.pid
+  AND m.cid(+) = c.cid
+  AND c.cid(+) = 1
+ORDER BY p.pid DESC, c.day DESC;
 
 ANSI-SQL
 SELECT product.pid, product.pnm, :cid cid, NVL(cycle.day, 0) day, NVL(cycle.cnt, 0) cnt
@@ -197,7 +206,7 @@ SELECT empno, ename, deptno
 FROM emp;
 
 SELECT empno, ename, deptno, 
-       (SELECT dname FROM dept WHERE deptno = emp.deptno) -- 상호연관 서브쿼리 (서브쿼리 따로 실행 불가)
+       (SELECT dname FROM dept WHERE deptno = emp.deptno) -- 상호연관 서브쿼리 (서브쿼리 따로 실행 불가) --한정자 필수
 FROM emp;                                                 -- 비상호연관은 따로 실행 가능.
 
 상호연관 서브쿼리 : 메인 쿼리의 컬럼을 서브쿼리에서 사용한 서브쿼리
@@ -214,5 +223,196 @@ SELECT *
 FROM dept
 WHERE deptno IN ( SELECT deptno
                   FROM emp);
-                
+                  
+전체 직원의 급여 평균보다 높은 급여를 받는 사원들 조회
+SELECT *
+FROM emp
+WHERE sal > (SELECT AVG(sal)
+             FROM emp);
+             
+본인 속한 부서의 급여 평균보다 높은 급여를 받는 사람들을 조회
+
+SELECT *
+FROM emp e
+WHERE  sal > (SELECT AVG(sal)
+             FROM emp
+             WHERE deptno = e.deptno);
+             
+             
+sub 4]
+테스트용 데이터 추가
+
+DESC dept;
+INSERT INTO dept VALUES (99, 'ddit', 'daejoen');
+
+SELECT *
+FROM dept
+WHERE deptno NOT IN (SELECT deptno
+FROM emp);
+
+SELECT deptno
+FROM emp;
+
+sub 5]
+
+SELECT *
+FROM product
+WHERE pid NOT IN (SELECT pid
+                  FROM cycle
+                  WHERE cid = 1);
+
+SELECT pid
+FROM cycle
+WHERE cid = 1;
+
+SELECT *
+FROM product;
+
+sub 6]
+
+SELECT *
+FROM cycle
+WHERE cid = 1
+  AND pid IN (SELECT pid
+            FROM cycle
+            WHERE cid = 2);
+            
+sub 7]
+
+SELECT c.cid, m.cnm, c.pid, p.pnm, c.day, c.cnt
+FROM customer m, product p, (SELECT *
+                            FROM cycle
+                            WHERE cid = 1
+                            AND pid IN (SELECT pid
+                                        FROM cycle
+                                        WHERE cid = 2)) c
+WHERE c.pid = p.pid  
+  AND c.cid = m.cid;
+ 
+ 
+EXISTS 연산자 : 조건을 만족하는 서브 쿼리의 행이 존재하면 TRUE . KEY!
+
+SELECT *
+FROM emp e
+WHERE EXISTS ( SELECT *
+               FROM emp m
+               WHERE e.mgr = m.empno);
+               
+sub 8]
+
+SELECT *
+FROM emp e, emp m
+WHERE e.mgr = m.empno;
+
+sub 9]
+
+SELECT *
+FROM product p
+WHERE EXISTS (SELECT *
+              FROM cycle c
+              WHERE c.pid = p.pid
+                AND c.cid = 1);
+
+sub 10]
+
+SELECT *
+FROM product p
+WHERE NOT EXISTS (SELECT *
+              FROM cycle c
+              WHERE c.pid = p.pid
+                AND c.cid = 1);
+
+집합 연산자 : 알아두자
+수학의 집합 연산
+A = { 1, 3, 5 }
+B = { 1, 4, 5 }
+
+합집합 : A U B = { 1, 3, 4, 5 } (교환법칙 성립) A U B == B U A
+교집합 : A ^ B = { 1, 5 } (교환법칙 성립) A ^ B == B ^ A
+차집합 : A - B = { 3 } (교환법칙 비성립) A - B != B - A
+        B - A = { 4 }
+        
+SQL에서의 집합 연산자
+합집합 : UNION      : 수학적 합집합과 개념이 동일(중복을 허용하지 않음)
+                     중복을 체크 ==> 두 집합에서 중복된 값을 확인 ==> 연산이 느림
+        UNION ALL  : 수학적 합집합 개념을 떠나 두개의 집합을 단순히 합친다.
+                     (중복 데이터 존재 가능)
+                     중복 체크 없음 ==> 두 집합에서 중복된 값 확인 없음 ==> 연산이 빠름
+                     **개발자가 두개의 집합에 중복되는 데이터가 없다는 것을 알 수 있는
+                     상황이라면 UNION연산자를 사용하는 것보다 UNION ALL을 사용하여 
+                     (오라클이 하는) 연산을 절약할 수 있다.
+       INTERSECT : 수학적 교집합 개념과 동일
+       MINUS : 수학적 차집합 개념과 동일 ;
+      
+위아래 집합이 동일하기 때문에 합집합을 하더라도 행이 추가되진 않는다.     
+SELECT empno, ename
+FROM emp
+WHERE deptno = 10
+UNION
+SELECT empno, ename
+FROM emp
+WHERE deptno = 10;
+
+
+위아래 집합에서 7369번 사번은 중복되므로 한번만 나오게 된다 : 전체 행은 3건    
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7566) 
+UNION
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7782);
+
+
+UNION ALL 연산자는 중복제거 단계가 없다. : 전체 행 4건
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7566) 
+UNION ALL
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7782);
+
+                    
+두 집합의 공통된 부분은 7369행 밖에 없음 : 총 데이터 1행
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7566) 
+INTERSECT
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7782);
+
+
+윗쪽 집합에서 아래쪽 집합의 행을 제거하고 남은 행 : 1개의 행 (7566)
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7566) 
+MINUS
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7782);
+
+
+집합연산자 특징
+1. 컬렴명은 첫번째 집합의 컬럼명을 따라간다
+2. order by 절은 마지막 집합에 적용한다
+   마지막 sql이 아닌 SQL에서 정렬을 사용하고 싶은 경우 INLINE-VIEW를 활용
+   UNION ALL의 경우 위, 아래 집합을 이어 주기 때문에 집합의 순서를 그댈 유지
+   하기 때문에 요구사항에 따라 정렬된 데이터 집합이 필요하다면 해당 방법을 고려
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7566) 
+UNION ALL
+SELECT empno, ename
+FROM emp
+WHERE empno IN ( 7369, 7782);
+
+
+
+
+
+
+
+
 
